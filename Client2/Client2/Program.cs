ef9177beb55182b634941cc793f585786d2f8363
harveymannering <60523103+harveymannering@users.noreply.github.com>
2021-01-08 11:44:25 +0000

    Inital commit

---
+++ Client2/Client2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client2
+{
+    class MainClass
+    {
+        private const int portNum = 15;
+        private const string hostName = "localhost";
+
+        public static int Main(String[] args)
+        {
+            Console.WriteLine("Enter String:");
+            string user_input = Console.ReadLine();
+
+            try
+            {
+                //establish connection
+                var client = new TcpClient(hostName, portNum);
+                NetworkStream ns = client.GetStream();
+
+                //send users string
+                byte[] byteSend = Encoding.ASCII.GetBytes(user_input);
+                try
+                {
+                    ns.Write(byteSend, 0, byteSend.Length);
+                                    }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+
+                //Read back response
+                byte[] bytes = new byte[1024];
+                int bytesRead = ns.Read(bytes, 0, bytes.Length);
+
+                Console.WriteLine(Encoding.ASCII.GetString(bytes, 0, bytesRead));
+
+                ns.Close();
+                client.Close();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ Server/Server/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    class MainClass
+    {
+        private const int portNum = 15;
+
+        public static void Main(string[] args)
+        {
+            bool done = false;
+
+            var listener = new TcpListener(IPAddress.Any, portNum);
+
+            listener.Start();
+
+            while (!done)
+            {
+                //Establish the connection
+                Console.Write("Waiting for connection...");
+                TcpClient client = listener.AcceptTcpClient();
+                Console.WriteLine("Connection accepted.");
+                NetworkStream ns = client.GetStream();
+
+                //acknowledge the receipt to the sending application
+                byte[] bytes = new byte[1024];
+                int bytesRead = ns.Read(bytes, 0, bytes.Length);
+                byte[] byteSend = Encoding.ASCII.GetBytes("String received");
+                try
+                {
+                    ns.Write(byteSend, 0, byteSend.Length);
+                    ns.Close();
+                    client.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+
+                //display the strings as items in a list.
+                string[] subs = Encoding.ASCII.GetString(bytes, 0, bytesRead).Split('|');
+                foreach (string s in subs)
+                {
+                    Console.WriteLine(s);
+                }
+
+            }
+
+            listener.Stop();
+
+        }
+    }
+}
